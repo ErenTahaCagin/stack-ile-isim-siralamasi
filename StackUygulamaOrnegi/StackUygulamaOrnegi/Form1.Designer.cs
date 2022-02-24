9158165bb4fa9fa97070aebdd3a4e6026433b2ca
ErenTahaCagin <100272957+ErenTahaCagin@users.noreply.github.com>
2022-02-24 08:54:29 +0300

    Add files via upload

---
+++ StackUygulamaOrnegi/StackUygulamaOrnegi/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace StackUygulamaOrnegi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnCikart = new System.Windows.Forms.Button();
+            this.lbStack = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(69, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad Soyad";
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(12, 30);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(192, 20);
+            this.txtAdSoyad.TabIndex = 1;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(12, 80);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(88, 36);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnCikart
+            // 
+            this.btnCikart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCikart.Location = new System.Drawing.Point(116, 80);
+            this.btnCikart.Name = "btnCikart";
+            this.btnCikart.Size = new System.Drawing.Size(88, 36);
+            this.btnCikart.TabIndex = 3;
+            this.btnCikart.Text = "Çıkart";
+            this.btnCikart.UseVisualStyleBackColor = true;
+            this.btnCikart.Click += new System.EventHandler(this.btnCikart_Click);
+            // 
+            // lbStack
+            // 
+            this.lbStack.FormattingEnabled = true;
+            this.lbStack.Location = new System.Drawing.Point(210, 9);
+            this.lbStack.Name = "lbStack";
+            this.lbStack.Size = new System.Drawing.Size(127, 186);
+            this.lbStack.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 206);
+            this.Controls.Add(this.lbStack);
+            this.Controls.Add(this.btnCikart);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Stack Uygulaması";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnCikart;
+        private System.Windows.Forms.ListBox lbStack;
+    }
+}
+
---
+++ StackUygulamaOrnegi/StackUygulamaOrnegi/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StackUygulamaOrnegi
+{
+    public partial class Form1 : Form
+    {
+        private Stack yigin = new Stack();    
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            yigin.Push(txtAdSoyad.Text);
+            Listele();
+        }
+
+        private void Listele()
+        {
+            lbStack.Items.Clear();
+            foreach (string eleman in yigin)
+            {
+                lbStack.Items.Add(eleman);
+            }
+        }
+
+        private void btnCikart_Click(object sender, EventArgs e)
+        {
+            yigin.Pop();
+            Listele();
+        }
+    }
+}
